099a7c07ff855a5ce4ea5fd059e8348aa838552b
cc <1442122744@qq.com>
2023-02-16 23:29:29 +0800

    feat: 添加gin相关

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"net/http"
 	"os"
 	"os/signal"
-	"syscall"
 
+	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
 
-func main() {
-	var ui lorca.UI
+//go:embed frontend/dist/*
+var FS embed.FS
 
-	ui, _ = lorca.New("https://baidu.com", "", 800, 600, "--disable-sync", "--disable-translate", "--disable-automation")
+func main() {
+	go func() {
+		gin.SetMode(gin.DebugMode)
+		r := gin.Default()
+		r.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{
+				"msg": "ok",
+			})
+		})
+		staticFils, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFils))
+		r.Run(":8082")
+	}()
+
+	ui, _ := lorca.New("http://127.0.0.1:8082/static/index.html", "", 800, 600, "--disable-sync", "--disable-translate", "--disable-automation")
 	chSignal := make(chan os.Signal, 1)
 
-	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(chSignal, os.Interrupt)
 
 	select {
 	case <-ui.Done():
